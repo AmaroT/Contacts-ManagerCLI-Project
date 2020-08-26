5e3f447e52b32169b4316949c1465f75f6190d1c
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-08-25 21:06:50 -0500

    test push

---
+++ src/TestJava.java
@@ -0,0 +1,6 @@
+public class TestJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("This repo is initialized and it runs ok");
+    }
+}
